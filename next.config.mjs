75228fe141a4a1fd104656884cb1939afa4607c1
erikerikssoncoding <erikeriksson.taicataikalsson@gmail.com>
2025-10-28 17:02:26 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     typedRoutes: true
-  }
+  },
+
+  // Adăugați această secțiune de mai jos
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.watchOptions = {
+        poll: 1000, // Verifică fișierele o dată pe secundă
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
